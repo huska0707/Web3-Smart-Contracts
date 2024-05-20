2651f7b00429090df08485d735cfb9bef70470c1
huska0707 <hussaink001haled@gmail.com>
2024-05-20 12:44:35 -0500

    Add function to set patient details and update mappings

--- contracts/Health-Rec/HealthRec.sol
+++ contracts/Health-Rec/HealthRec.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 contract Record {
@@ -50,17 +51,46 @@ contract Record {
     address[] public doctorList;
     address[] public appointmentList;
 
-    mapping(address => mapping(address => bool)) isApproved;
     mapping(address => Patients) patients;
     mapping(address => Doctors) doctors;
     mapping(address => Appointments) appointments;
+
+    mapping(address=>mapping(address=>bool)) isApproved;
+    mapping(address => bool) isPatient;
+    mapping(address => bool) isDoctor;
+    mapping(address => uint) AppointmentPerPatient;
 
     uint256 public patientCount = 0;
     uint256 public doctorCount = 0;
     uint256 public appointmentCount = 0;
     uint256 public permissionGrantedCount = 0;
-    
+
     constructor() {
         owner = msg.sender;
+    }
+
+    //Retrieve patient details from user sign up page and store the details into the blockchain
+    function setDetails(string memory _ic, string memory _name, string memory _phone, string memory _gender, string memory _dob, string memory _height, string memory _weight, string memory _houseaddr, string memory _bloodgroup, string memory _allergies, string memory _medication) public {
+        require(!isPatient[msg.sender]);
+        Patients storage p = patients[msg.sender];
+        
+        p.ic = _ic;
+        p.name = _name;
+        p.phone = _phone;
+        p.gender = _gender;
+        p.dob = _dob;
+        p.height = _height; 
+        p.weight = _weight;
+        p.houseaddr = _houseaddr;
+        p.bloodgroup = _bloodgroup;
+        p.allergies = _allergies;
+        p.medication = _medication;
+        p.addr = msg.sender;
+        p.date = block.timestamp;
+        
+        patientList.push(msg.sender);
+        isPatient[msg.sender] = true;
+        isApproved[msg.sender][msg.sender] = true;
+        patientCount++;
     }
 }
