f5f4b90fb229ac00b2ca6187800c34bed474c3e1
huska0707 <hussaink001haled@gmail.com>
2024-05-20 12:32:47 -0500

    Add data structures and mappings for patients, doctors, and appointments

--- contracts/Health-Rec/HealthRec.sol
+++ contracts/Health-Rec/HealthRec.sol
@@ -1,11 +1,65 @@
 pragma solidity ^0.8.0;
 
 contract Record {
+
+    struct Patients{
+        string ic;
+        string name;
+        string phone;
+        string gender;
+        string dob;
+        string height;
+        string weight;
+        string houseaddr;
+        string bloodgroup;
+        string allergies;
+        string medication;
+        string emergencyName;
+        string emergencyContact;
+        address addr;
+        uint date;
+    }
+
+    struct Doctors{
+        string ic;
+        string name;
+        string phone;
+        string gender;
+        string dob;
+        string qualification;
+        string major;
+        address addr;
+        uint date;
+    }
+
+    struct Appointments{
+        address doctoraddr;
+        address patientaddr;
+        string date;
+        string time;
+        string prescription;
+        string description;
+        string diagnosis;
+        string status;
+        uint creationDate;
+    }
+
     address public owner;
+
     address[] public patientList;
     address[] public doctorList;
     address[] public appointmentList;
 
+    mapping(address => mapping(address => bool)) isApproved;
+    mapping(address => Patients) patients;
+    mapping(address => Doctors) doctors;
+    mapping(address => Appointments) appointments;
+
+    uint256 public patientCount = 0;
+    uint256 public doctorCount = 0;
+    uint256 public appointmentCount = 0;
+    uint256 public permissionGrantedCount = 0;
+    
     constructor() {
         owner = msg.sender;
     }
