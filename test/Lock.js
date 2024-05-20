efe2ac2f8ee541016d6a87d19f2b76a196196508
huska0707 <hussaink001haled@gmail.com>
2024-05-19 22:13:18 -0500

    Add test fixture for deploying Lock contract with one-year lock period in Test Script

---
+++ test/Lock.js
@@ -0,0 +1,22 @@
+const {
+  time,
+  loadFixture,
+} = require("@nomicfoundation/hardhat-network-helpers");
+const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
+const { expect } = require("chai");
+
+describe("Lock", function () {
+  async function deployOneYearLockFixture() {
+    const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+    const ONE_GWEI = 1_000_000_000;
+
+    const lockedAmount = ONE_GWEI;
+    const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
+    // Contracts are deployed using the first signer/account by default
+    const [owner, otherAccount] = await ethers.getSigners();
+
+    const Lock = await ethers.getContractFactory("Lock");
+    const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+    return { lock, unlockTime, lockedAmount, owner, otherAccount };
+  }
+});
