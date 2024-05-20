e4b373c1d2943af773b1de530bde50ad31314955
huska0707 <hussaink001haled@gmail.com>
2024-05-19 21:28:12 -0500

    Function to deposit ether into the account of sender

--- contracts/Banking/BankingSystem.sol
+++ contracts/Banking/BankingSystem.sol
@@ -1,5 +1,9 @@
 pragma solidity ^0.8.0
 
 contract BankingSystem {
-    
+    mapping(address => uint256) public balanceOf;
+
+    function deposit() public payable {
+        balanceOf[msg.sender] += msg.value;
+    }
 }
