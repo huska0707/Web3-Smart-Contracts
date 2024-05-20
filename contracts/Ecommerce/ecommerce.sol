20b40632ce7af2b1bda7ed096558b9e7cec3cb4a
huska0707 <hussaink001haled@gmail.com>
2024-05-20 12:15:34 -0500

    Add product registration functionality with event emission

--- contracts/Ecommerce/ecommerce.sol
+++ contracts/Ecommerce/ecommerce.sol
@@ -12,5 +12,23 @@ contract Ecommerce {
 
     Product [] public products;
     uint counter = 0;
+
+    event registered(string title, uint productId, address seller);
     
+    function registerProduct
+        (string memory _title,
+        string memory _description,
+        uint _price)
+        public {
+            require(_price > 0 , "Price of products has to be greater then 0");
+            Product memory tempProduct;
+            tempProduct.title = _title;
+            tempProduct.description = _description;
+            tempProduct.price = _price * 10**18;
+            tempProduct.seller = payable(msg.sender);
+            tempProduct.productId = counter;
+            products.push(tempProduct);
+            counter++;
+            emit registered(_title,tempProduct.productId, msg.sender);
+        } 
 }
