5f425b6e1dbc757952e94cf49a78959e209aff06
huska0707 <hussaink001haled@gmail.com>
2024-05-23 09:36:40 -0500

    Added function get repuation and post

--- contracts/DAO/metadao.sol
+++ contracts/DAO/metadao.sol
@@ -150,4 +150,23 @@ contract MetaDAO {
     ) public view returns (string memory) {
         return categoryRegistry[_categoryId];
     }
+
+    function getReputation(
+        address _address,
+        bytes32 _categoryID
+    ) public view returns (uint80) {
+        return reputationRegistry[_address][_categoryID];
+    }
+
+    function getPost(
+        bytes32 _postId
+    ) public view returns (address, bytes32, bytes32, int72, bytes32) {
+        return (
+            postRegistry[_postId].postOwner,
+            postRegistry[_postId].parentPost,
+            postRegistry[_postId].contentId,
+            postRegistry[_postId].votes,
+            postRegistry[_postId].categoryId
+        );
+    }
 }
